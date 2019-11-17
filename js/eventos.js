2cca88f023467276f294291626d5cecc76aa925c
renan <renanalves23@outlook.com>
2019-11-17 19:44:03 -0300

    Commit Inicial]

---
+++ js/eventos.js
@@ -0,0 +1,13 @@
+      
+
+document.getElementById("click-me").onclick = () => {
+  alert('Voce clicou em mim')
+};
+
+document.getElementById("hover-me").onmouseover = () => {
+  alert('Passou sobre mim')
+};
+
+document.getElementById("leave-me").onmouseleave = () => {
+  alert('Nao se vá')
+}
