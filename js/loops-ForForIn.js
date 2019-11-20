e0e8b4196594f633869e246d34e3c102c942005b
renan <renanalves23@outlook.com>
2019-11-20 17:04:01 -0300

    Commit add For Loop and For In Loop

---
+++ js/loops-ForForIn.js
@@ -0,0 +1,45 @@
+
+ 
+
+ //FORLOOP
+
+ var alunos = ['Pedro', 'Maria', 'José', 'João', 'Carlos'];
+
+for(let i = 0; i < alunos.length; i++){
+ // console.log(alunos[i])
+}
+
+
+//FOR IN
+
+var carro = {
+  'Ano': 2018,
+  'Modelo': 'Fox',
+  'Cilindradas': '1.8',
+  'Combustível': 'Gasolina'
+};
+
+for (let prop in carro) {
+  console.log(`${prop} : ${carro[prop]}`)
+}
+
+
+//EXEMPLO DA AULA ANTERIOR PARA FAZER LOOP
+
+  const elementos = document.getElementsByClassName("exemplo") //array com 3 elementos
+
+  for(j = 0; j < elementos.length; j++){
+    elementos[j].style.color = "orange";
+    elementos[j].style['font-weight'] = "bold";
+  }
+
+  const elementos2 = document.getElementsByTagName("p") 
+
+  for(j = 0; j < elementos2.length; j++){
+    elementos2[j].style.color = "orange";
+    elementos2[j].style['font-weight'] = "bold";
+  };
+
+
+
+
