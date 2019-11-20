606b685259e379cf7b05687ecb9d172cf82d61d5
renan <renanalves23@outlook.com>
2019-11-20 16:39:23 -0300

    Commit add outrosGetElement

---
+++ js/outros-get-element.js
@@ -0,0 +1,15 @@
+
+
+const exemplo = document.getElementsByClassName('exemplo');
+      exemplo[0].innerHTML = "teste"
+
+      //ById retorna um objeto
+      //ByClassName retorna um Array
+
+
+      //ByTagName
+
+
+const exemplo2 = document.getElementsByTagName("p");
+
+console.log(exemplo2);
